0b068c6d5599a8c84a69f03d56f8195a8ff49bdd
pbcarloz <pb_carlos@outlook.com>
2020-04-08 12:59:57 -0500

    Saving Changes

--- Ejercicios.js
+++ Ejercicios.js
@@ -9,3 +9,15 @@ const checkIs100 = (a, b) => {
 // solucion chingona
 
 const checkIf100 = (a,b) => a === 100 || b === 100 || (a + b) === 100;
+
+// Write a JavaScript program to get the extension of a filename.
+
+const getFileExtension = (str) => str.slice(str.lastIndexOf("."));
+
+// write a function to replace every character in a given string with the character following it in the alphabet.
+
+const moveChharsForward = (str) =>
+    str
+    .spli("")
+    .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+    .join("");
